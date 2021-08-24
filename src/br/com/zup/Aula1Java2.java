fc4b3eb136056e709c341744b3b288286806f8ab
Let Marçal <leticiamarcalmatias@gmail.com>
2021-08-24 14:45:34 -0300

    Operadores matemáticos

--- src/br/com/zup/Aula1Java2.java
+++ src/br/com/zup/Aula1Java2.java
@@ -6,8 +6,12 @@ public class Aula1Java2 {
         int numero1 = 20;
         int numero2 = 20;
 
+        //criando a operação de soma
         int resultadoDaSoma = numero1 + numero2;
-        System.out.println(resultadoDaSoma);
+        System.out.println("O resultado da soma é: " + resultadoDaSoma);
 
+        //criando a operação de subtração
+        int resultadoDaSubtracao = numero1 - numero2;
+        System.out.println("O resultado da subtração é: " + resultadoDaSubtracao);
     }
 }
